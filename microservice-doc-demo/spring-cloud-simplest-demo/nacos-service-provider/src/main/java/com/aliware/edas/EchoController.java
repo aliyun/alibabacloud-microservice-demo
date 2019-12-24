fd5f1a786feb07992429415d4eb0d9b8468ec836
亦盏 <yizhan.xj@alibaba-inc.com>
2019-12-24 16:12:11 +0800

    删除sleep逻辑

--- microservice-doc-demo/spring-cloud-simplest-demo/nacos-service-provider/src/main/java/com/aliware/edas/EchoController.java
+++ microservice-doc-demo/spring-cloud-simplest-demo/nacos-service-provider/src/main/java/com/aliware/edas/EchoController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author edas
@@ -14,13 +13,6 @@ import java.util.concurrent.TimeUnit;
 public class EchoController {
 	@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
 	public String echo(@PathVariable String string) {
-		try {
-
-			TimeUnit.SECONDS.sleep(60);
-		}
-		catch (Exception e) {
-
-		}
 		return string;
 	}
 }
