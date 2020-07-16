1c3cdac3631cb0fde955c307a023ba136487f265
lym-ifae <lym_phy@163.com>
2020-07-16 16:10:09 +0800

    add swagger annotations

--- src/frontend/src/main/java/com/alibabacloud/hipstershop/web/AppController.java
+++ src/frontend/src/main/java/com/alibabacloud/hipstershop/web/AppController.java
@@ -4,6 +4,9 @@ import com.alibabacloud.hipstershop.CartItem;
 import com.alibabacloud.hipstershop.dao.CartDAO;
 import com.alibabacloud.hipstershop.dao.ProductDAO;
 import com.alibabacloud.hipstershop.domain.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,7 @@ import java.util.Random;
 /**
  * @author wangtao 2019-08-12 15:41
  */
+@Api(value = "/", tags = {"首页操作接口"})
 @Controller
 public class AppController {
 
@@ -33,6 +37,7 @@ public class AppController {
 
     private String userID = "Test User";
 
+    @ApiOperation(value = "首页", tags = {"首页操作接口"})
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("products", productDAO.getProductList());
@@ -58,6 +63,7 @@ public class AppController {
         return "home";
     }
 
+    @ApiOperation(value = "设置用户id")
     @GetMapping("/setUser")
     public String user(@RequestParam(name="userId", required=false) String userId, Model model) {
         userID = userId;
@@ -88,13 +94,15 @@ public class AppController {
         return "not support yet";
     }
 
+    @ApiOperation(value = "产品详情", tags = {"用户操作接口"})
     @GetMapping("/product/{id}")
-    public String product(@PathVariable(name="id") String id, Model model) {
+    public String product(@PathVariable(name="id") @ApiParam(name = "id", value = "产品id", required = true) String id, Model model) {
         Product p = productDAO.getProductById(id);
         model.addAttribute("product", p);
         return "product.html";
     }
 
+    @ApiOperation(value = "查看购物车")
     @GetMapping("/cart")
     public String viewCart(Model model) {
         List<CartItem> items = cartDAO.viewCart(userID);
@@ -108,9 +116,10 @@ public class AppController {
         return "cart.html";
     }
 
+    @ApiOperation(value = "新增购物车商品，其中有蒸羊羔、蒸熊掌、蒸鹿尾儿、烧花鸭、烧雏鸡、烧子鹅、卤猪、卤鸭、酱鸡、腊肉、松花小肚儿、晾肉、香肠、什锦苏盘、熏鸡白肚儿、清蒸八宝猪、江米酿鸭子、罐儿野鸡、罐儿鹌鹑、卤什锦、卤子鹅、山鸡、兔脯、菜蟒、银鱼、清蒸哈什蚂、烩鸭丝、烩鸭腰、烩鸭条、清拌鸭丝、黄心管儿、焖白鳝、焖黄鳝、豆豉鲇鱼、锅烧鲤鱼、锅烧鲇鱼、清拌甲鱼、抓炒鲤鱼、抓炒对虾、软炸里脊、软炸鸡、什锦套肠儿、卤煮寒鸭儿、麻酥油卷儿、熘鲜蘑、熘鱼脯、熘鱼肚、熘鱼片儿、醋熘肉片儿、烩三鲜、烩白蘑、烩鸽子蛋、炒银丝、烩鳗鱼、炒白虾、炝青蛤、炒面鱼、炒竹笋、芙蓉燕菜、炒虾仁、烩虾仁、烩腰花儿、烩海参、炒蹄筋儿、锅烧海参、锅烧白菜、炸木耳、炒肝尖儿、桂花翅子、清蒸翅子、炸飞禽、炸汁儿、炸排骨、清蒸江瑶柱、糖熘勾芡仁米、拌鸡丝、拌肚丝、什锦豆腐、什锦丁儿、糟鸭、糟熘鱼片儿、熘蟹肉、炒蟹肉、烩蟹肉、清拌蟹肉、蒸南瓜、酿倭瓜、炒丝瓜、酿冬瓜、烟鸭掌儿、焖鸭掌儿、焖笋、炝茭白、茄子晒炉肉、鸭羹、蟹肉羹、鸡血汤、三鲜木樨汤、红丸子、白丸子、南煎丸子、四喜丸子、三鲜丸子、汆丸子、鲜虾丸子、鱼脯丸子、饹炸丸子、豆腐丸子、樱桃肉、马牙肉、米粉肉、一品肉、栗子肉、坛子肉、红焖肉、黄焖肉、酱豆腐肉、晒炉肉、炖肉、黏糊肉、烀肉、扣肉、松肉、罐儿肉、烧肉、大肉、烤肉、白肉、红肘子、白肘子、熏肘子、水晶肘子、蜜蜡肘子、锅烧肘子、扒肘条、炖羊肉、酱羊肉、烧羊肉、烤羊肉、清羔羊肉、五香羊肉、氽三样、爆三样、炸卷果儿、烩散丹、烩酸燕儿、烩银丝、烩白杂碎、氽节子、烩节子、炸绣球、三鲜鱼翅、栗子鸡、氽鲤鱼、酱汁鲫鱼、活钻鲤鱼、板鸭、筒子鸡、烩脐肚、烩南荠、爆肚仁儿、盐水肘花儿、锅烧猪蹄儿、拌稂子、炖吊子、烧肝尖儿、烧肥肠儿、烧心、烧肺、烧紫盖儿、烧连帖、烧宝盖儿、油炸肺、酱瓜丝儿、山鸡丁、拌海蜇、龙须菜、炝冬笋、玉兰片、烧鸳鸯、烧鱼头、烧槟子、烧百合、炸豆腐、炸面筋、炸软巾、糖熘饹、拔丝山药、糖焖莲子、酿山药、杏仁儿酪、小炒螃蟹、氽大甲、炒荤素儿、什锦葛仙米、鳎目鱼、八代鱼、海鲫鱼、黄花鱼、鲥鱼、带鱼、扒海参、扒燕窝、扒鸡腿儿、扒鸡块儿、扒肉、扒面筋、扒三样儿、油泼肉、酱泼肉、炒虾黄、熘蟹黄、炒子蟹、炸子蟹、佛手海参、炸烹儿、炒芡子米、奶汤、翅子汤、三丝汤、熏斑鸠、卤斑鸠、海白米、烩腰丁儿、火烧茨菰、炸鹿尾儿、焖鱼头、拌皮渣儿、氽肥肠儿、炸紫盖儿、鸡丝豆苗、十二台菜、汤羊、鹿肉、驼峰、鹿大哈、插根儿、炸花件儿，清拌粉皮儿、炝莴笋、烹芽韭、木樨菜、烹丁香、烹大肉、烹白肉、麻辣野鸡、烩酸蕾、熘脊髓、咸肉丝儿、白肉丝儿、荸荠一品锅、素炝春不老、清焖莲子、酸黄菜、烧萝卜、脂油雪花儿菜、烩银耳、炒银枝儿、八宝榛子酱、黄鱼锅子、白菜锅子、什锦锅子、汤圆锅子、菊花锅子、杂烩锅子、煮饽饽锅子、肉丁辣酱、炒肉丝、炒肉片儿、烩酸菜、烩白菜、烩豌豆、焖扁豆、氽毛豆、炒豇豆，外加腌苤蓝丝儿。")
     @PostMapping("/cart")
-    public RedirectView addToCart(@RequestParam(name="product_id") String productID,
-                                  @RequestParam(name="quantity") int quantity) {
+    public RedirectView addToCart(@RequestParam(name="product_id") @ApiParam(name = "productID", value = "产品id", required = true) String productID,
+                                  @RequestParam(name="quantity") @ApiParam(name = "quantity", value = "数量", required = true) int quantity) {
         cartDAO.addToCart(userID, productID, quantity);
         return new RedirectView("/cart");
     }
