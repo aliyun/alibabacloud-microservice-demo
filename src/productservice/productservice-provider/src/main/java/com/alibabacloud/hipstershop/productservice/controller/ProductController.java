22b044ad69315c00397b129e469d82c116526884
亦盏 <yizhan.xj@alibaba-inc.com>
2020-09-25 11:44:56 +0800

    在 helm chart 中增加 mysql 和 redis

--- src/productservice/productservice-provider/src/main/java/com/alibabacloud/hipstershop/productservice/controller/ProductController.java
+++ src/productservice/productservice-provider/src/main/java/com/alibabacloud/hipstershop/productservice/controller/ProductController.java
@@ -1,11 +1,16 @@
 package com.alibabacloud.hipstershop.productservice.controller;
 
+import com.alibabacloud.hipstershop.productservice.ProductServiceApplication;
 import com.alibabacloud.hipstershop.productservice.entity.ProductInfo;
 import com.alibabacloud.hipstershop.productservice.service.ProductServiceApi;
 import com.alibabacloud.hipstershop.productserviceapi.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,6 +20,16 @@ public class ProductController {
 
     @Resource
     ProductServiceApi productServiceApi;
+
+    @Autowired
+    private Registration registration;
+
+    @ModelAttribute
+    public void setVaryResponseHeader(HttpServletResponse response) {
+        response.setHeader("APP_NAME", ProductServiceApplication.APP_NAME);
+        response.setHeader("SERVICE_TAG", ProductServiceApplication.SERVICE_TAG);
+        response.setHeader("SERVICE_IP", registration.getHost());
+    }
 
     @GetMapping("/product/{id}")
     public Product getProductById(@PathVariable(name = "id") String id) {
