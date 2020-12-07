a25b393d7c5a175b8b2c37188ad068f25f73c834
lexburner <xujingfeng95@gmail.com>
2020-12-07 10:20:02 +0800

    remove time wait

--- microservice-doc-demo/dubbo-samples-spring-boot/dubbo-spring-boot-provider/src/main/java/com/alibaba/edas/boot/EchoServiceImpl.java
+++ microservice-doc-demo/dubbo-samples-spring-boot/dubbo-spring-boot-provider/src/main/java/com/alibaba/edas/boot/EchoServiceImpl.java
@@ -1,8 +1,5 @@
 package com.alibaba.edas.boot;
 
-
-import java.util.concurrent.TimeUnit;
-
 import org.apache.dubbo.config.annotation.Service;
 
 @Service(version = "1.0.0", group = "DUBBO")
@@ -10,17 +7,7 @@ public class EchoServiceImpl implements EchoService {
 
     @Override
     public String echo(String name) {
-        long start = System.currentTimeMillis();
-
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-        }
-
-        long end = System.currentTimeMillis();
-        return "\r\n\t" + start + " Provider received." +
-                "\r\n\t\tProvider processed after sleep 1 second! Echo String: \"" + name + "\"" +
-                "\r\n\t" + end + " Provider Return";
+        return "hello" + name;
 
     }
 }
