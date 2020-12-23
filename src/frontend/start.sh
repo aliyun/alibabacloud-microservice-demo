2254e15e440c5729dcddb95aa741a0ffe6485136
luyanbo <robert.lyb@alibaba-inc.com>
2020-12-23 13:45:32 +0800

    chmod a+x start.sh

--- src/frontend/start.sh
+++ src/frontend/start.sh
@@ -4,4 +4,4 @@ java \
   -Dcom.sun.management.jmxremote.port=19991 \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -Dcom.sun.management.jmxremote.ssl=false \
-  -jar /app/exception-mock-provider-1.0.0-SNAPSHOT.jar
+  -jar /app/frontend-1.0.0-SNAPSHOT.jar
