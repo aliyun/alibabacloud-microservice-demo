320194bcf79519420e6b05aad14247c11b4e329f
亦盏 <yizhan.xj@alibaba-inc.com>
2022-04-07 20:01:54 +0800

    delete comments

--- mse-simple-demo/C/src/main/java/com/alibabacloud/mse/demo/mq/RocketMqConfiguration.java
+++ mse-simple-demo/C/src/main/java/com/alibabacloud/mse/demo/mq/RocketMqConfiguration.java
@@ -1,11 +1,5 @@
 package com.alibabacloud.mse.demo.mq;
 
-/**
- * @author wangbb
- * @version 1.0
- * @description: rocketmq容器加载
- * @date 2021/8/10 10:10 上午
- */
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @author zhujy
- */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
