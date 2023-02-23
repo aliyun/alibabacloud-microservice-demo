ff379d38cad1037b4cb770a4cf897c0b1054c4dd
LearningGp <learninggp4.74@gmail.com>
2023-02-23 12:02:25 +0800

    add --platform linux/amd64

--- mse-simple-demo/C/build.sh
+++ mse-simple-demo/C/build.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-docker build . -t ${REGISTRY}spring-cloud-c:1.2.0
+docker build --platform linux/amd64 . -t ${REGISTRY}spring-cloud-c:1.2.0
 
 if [ -n "${REGISTRY}" ]; then
     docker push ${REGISTRY}spring-cloud-c:1.2.0
