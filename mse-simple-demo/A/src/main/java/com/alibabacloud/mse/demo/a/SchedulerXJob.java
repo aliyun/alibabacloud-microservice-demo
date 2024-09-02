5158a1e9a6f6d694cc107d733ed857f76a8e56f6
EndlessSeeker <1766508902@qq.com>
2024-09-02 16:32:10 +0800

    fix: component

--- mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/SchedulerXJob.java
+++ mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/SchedulerXJob.java
@@ -1,11 +1,12 @@
 package com.alibabacloud.mse.demo.a;
 
+import org.springframework.stereotype.Component;
 import com.alibaba.schedulerx.worker.domain.JobContext;
 import com.alibaba.schedulerx.worker.processor.JavaProcessor;
 import com.alibaba.schedulerx.worker.processor.ProcessResult;
 import com.alibabacloud.mse.demo.a.AController;
 
-@Controller
+@Component
 public class SchedulerXJob extends JavaProcessor {
     @Autowired
     private AController aController;
