134f96c4f22986c3230d5c63c938df7ed9df2801
EndlessSeeker <1766508902@qq.com>
2024-09-02 16:28:14 +0800

    feat: update schedule logic

--- mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/SchedulerXJob.java
+++ mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/SchedulerXJob.java
@@ -3,12 +3,20 @@ package com.alibabacloud.mse.demo.a;
 import com.alibaba.schedulerx.worker.domain.JobContext;
 import com.alibaba.schedulerx.worker.processor.JavaProcessor;
 import com.alibaba.schedulerx.worker.processor.ProcessResult;
+import com.alibabacloud.mse.demo.a.AController;
 
-@Component
+@Controller
 public class SchedulerXJob extends JavaProcessor {
+    @Autowired
+    private AController aController;
+
     @Override
     public ProcessResult process(JobContext context) throws Exception {
-        System.out.println("hello schedulerx2.0");
+        System.out.println(context);
+
+        String result = aController.a();
+        System.out.println(result)
+
         return new ProcessResult(true);
     }
 }
