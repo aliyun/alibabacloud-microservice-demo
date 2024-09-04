b3780095651b1de3f90894d0090d6c130e60ad8d
EndlessSeeker <1766508902@qq.com>
2024-09-04 11:16:12 +0800

    fix: application start fail

--- mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/SchedulerXJob.java
+++ mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/SchedulerXJob.java
@@ -6,19 +6,21 @@ import com.alibaba.schedulerx.worker.processor.JavaProcessor;
 import com.alibaba.schedulerx.worker.processor.ProcessResult;
 import com.alibabacloud.mse.demo.a.AController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class SchedulerXJob extends JavaProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SchedulerXJob.class);
+
     @Autowired
     private AController aController;
 
     @Override
     public ProcessResult process(JobContext context) throws Exception {
-        System.out.println(context);
-
+        log.Info("shceduler x job context is: {}", context)
         String result = aController.a();
-        System.out.println(result);
-
+        log.Info("scheduler x job run result is: {}", result)
         return new ProcessResult(true);
     }
 }
