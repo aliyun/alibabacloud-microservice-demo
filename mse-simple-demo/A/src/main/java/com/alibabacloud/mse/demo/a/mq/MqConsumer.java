19b57866ab835e1132aab59ad945b20c3ff1e737
亦盏 <yizhan.xj@alibaba-inc.com>
2024-12-17 20:00:54 +0800

    update spring boot version to latest

--- mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/mq/MqConsumer.java
+++ mse-simple-demo/A/src/main/java/com/alibabacloud/mse/demo/a/mq/MqConsumer.java
@@ -9,7 +9,6 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
